0deda87e38ed774cd7acab09f18b7c8d8b22e9e3
umesh <usp5758@gmail.com>
2020-05-14 11:29:46 +0530

    case uc4 push

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,20 +2,28 @@
 
 heads=0
 tails=0
+
+flipCoin() {
+	result=$((RANDOM % 2))
+        if [ $result -eq 0 ]
+        then
+                $((tails++))
+        else
+                $((heads++))
+        fi
+}
+
 while [ $tails -le 21 -o $heads -le 21 ]
 do
-	result=$((RANDOM % 2))
-	if [ $result -eq 0 ]
-	then
-		$((tails++))
-	else
-		$((heads++))
-	fi
+	flipCoin
 done
 
 if [$heads -eq $tails ]
 then
-	echo "Its a tie !"
+	while [ $(($heads - $tails)) -ne 2 -o $(($tails - $heads)) -ne 2 ]
+	do
+		flipCoin
+	done
 elif [ $heads -gt $tails ]
 then
 	echo "Heads Wins !"
