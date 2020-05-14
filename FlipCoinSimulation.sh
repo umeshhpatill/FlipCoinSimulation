94f9dcc005a70be04252bb5ccbb913d53168cad0
umesh <usp5758@gmail.com>
2020-05-14 11:25:24 +0530

    case uc2

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,9 +1,19 @@
 #!/bin/bash -x
 
-result=$((RANDOM % 2))
-if [ $result -eq 0 ]
-then
-	echo "Tails"
-else
-	echo "Heads"
-fi
+read -p "Enter Times to flip: " num
+heads=0
+tails=0
+count=0
+while [ $count -ne $num ]
+do
+	$((count++))
+	result=$((RANDOM % 2))
+	if [ $result -eq 0 ]
+	then
+		$((tails++))
+	else
+		$((heads++))
+	fi
+done
+echo "Heads: " $heads
+echo "Tails: " $tails
